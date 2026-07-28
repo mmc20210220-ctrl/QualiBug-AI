2adb11d6ae30032fc3a91e83a2cd4211fe6916d5
mmc20210220-ctrl <mmc20210220@gmail.com>
2026-07-29 07:53:46 +0800

    feat(document-ingestion): export image decoder extension API

--- ai_test_asset_center/enterprise_knowledge_center/document_ingestion/__init__.py
+++ ai_test_asset_center/enterprise_knowledge_center/document_ingestion/__init__.py
@@ -5,6 +5,17 @@ from .contract import (
     DocumentSource,
     SupplementalContext,
 )
+from .image_decoding import (
+    CairoSvgImageDecoder,
+    DecodedImageFrame,
+    ImageDecodeBatch,
+    ImageDecoder,
+    ImageDecoderRegistry,
+    PillowImageDecoder,
+    RawpyCameraImageDecoder,
+    build_default_image_decoder_registry,
+    sniff_image_source,
+)
 from .ocr_adapter import OcrProvider, TesseractOcrProvider
 from .page_render_registry import PageRendererRegistry, build_default_page_renderer_registry
 from .page_rendering import (
@@ -19,6 +30,7 @@ from .pipeline import build_document_structure_ir
 from .planner import plan_deferred_supplementals, plan_document_parsing
 from .registry import DocumentAdapterRegistry, build_default_registry
 from .rendered_ocr_adapter import OcrSupplementalAdapter
+from .universal_image_renderer import UniversalImagePageRenderer
 
 __all__ = [
     "AdapterMatch",
@@ -28,6 +40,16 @@ __all__ = [
     "OcrProvider",
     "OcrSupplementalAdapter",
     "TesseractOcrProvider",
+    "DecodedImageFrame",
+    "ImageDecodeBatch",
+    "ImageDecoder",
+    "ImageDecoderRegistry",
+    "PillowImageDecoder",
+    "CairoSvgImageDecoder",
+    "RawpyCameraImageDecoder",
+    "UniversalImagePageRenderer",
+    "build_default_image_decoder_registry",
+    "sniff_image_source",
     "RenderedPage",
     "PageRenderBatch",
     "PageRendererRegistry",
