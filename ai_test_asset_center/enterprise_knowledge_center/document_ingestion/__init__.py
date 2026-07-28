d2fc37f541d8076cf070a3b0ac82ab3e29833ea4
mmc20210220-ctrl <mmc20210220@gmail.com>
2026-07-28 22:59:52 +0800

    Export deferred OCR ingestion API

--- ai_test_asset_center/enterprise_knowledge_center/document_ingestion/__init__.py
+++ ai_test_asset_center/enterprise_knowledge_center/document_ingestion/__init__.py
@@ -1,15 +1,26 @@
 """Format-agnostic enterprise document ingestion."""
-from .contract import AdapterMatch, DocumentAdapter, DocumentSource
+from .contract import (
+    AdapterMatch,
+    DocumentAdapter,
+    DocumentSource,
+    SupplementalContext,
+)
+from .ocr_adapter import OcrProvider, OcrSupplementalAdapter, TesseractOcrProvider
 from .pipeline import build_document_structure_ir
-from .planner import plan_document_parsing
+from .planner import plan_deferred_supplementals, plan_document_parsing
 from .registry import DocumentAdapterRegistry, build_default_registry
 
 __all__ = [
     "AdapterMatch",
     "DocumentAdapter",
     "DocumentSource",
+    "SupplementalContext",
+    "OcrProvider",
+    "OcrSupplementalAdapter",
+    "TesseractOcrProvider",
     "DocumentAdapterRegistry",
     "build_default_registry",
     "plan_document_parsing",
+    "plan_deferred_supplementals",
     "build_document_structure_ir",
 ]
