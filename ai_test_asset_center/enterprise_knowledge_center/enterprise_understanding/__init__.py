151038378d59d9c42bbc41049be632ed8fb3a021
mmc20210220-ctrl <mmc20210220@gmail.com>
2026-07-29 10:44:38 +0800

    feat(enterprise-understanding): export Business Behavior IR compiler

--- ai_test_asset_center/enterprise_knowledge_center/enterprise_understanding/__init__.py
+++ ai_test_asset_center/enterprise_knowledge_center/enterprise_understanding/__init__.py
@@ -1,4 +1,12 @@
 """Enterprise business understanding model package."""
+from .behavior_ir import (
+    BEHAVIOR_GATE_SCHEMA,
+    BEHAVIOR_ROW_LEDGER_SCHEMA,
+    BEHAVIOR_SCHEMA,
+    build_business_behavior_ir,
+    build_decision_matrix_row_ledger,
+)
+from .behavior_ir_governance import build_governed_business_behavior_ir
 from .builder import build_enterprise_understanding_model
 from .gate import assess_understanding_model
 from .integration import (
@@ -10,6 +18,12 @@ from .object_graph import build_object_graph
 from .schema import *  # noqa: F401,F403
 
 __all__ = [
+    "BEHAVIOR_SCHEMA",
+    "BEHAVIOR_ROW_LEDGER_SCHEMA",
+    "BEHAVIOR_GATE_SCHEMA",
+    "build_decision_matrix_row_ledger",
+    "build_business_behavior_ir",
+    "build_governed_business_behavior_ir",
     "build_enterprise_understanding_model",
     "assess_understanding_model",
     "enrich_asset_with_enterprise_understanding",
