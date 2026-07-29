1da6019a0582e506f3b7d6528d974afb71518ee4
mmc20210220-ctrl <mmc20210220@gmail.com>
2026-07-29 11:08:06 +0800

    feat(enterprise-understanding): export implementation binding API

--- ai_test_asset_center/enterprise_knowledge_center/enterprise_understanding/__init__.py
+++ ai_test_asset_center/enterprise_knowledge_center/enterprise_understanding/__init__.py
@@ -10,6 +10,11 @@ from .behavior_ir_governance import build_governed_business_behavior_ir
 from .behavior_ir_logic_gate import build_business_behavior_ir_v1
 from .builder import build_enterprise_understanding_model
 from .gate import assess_understanding_model
+from .implementation_binding import (
+    IMPLEMENTATION_BINDING_GATE_SCHEMA,
+    IMPLEMENTATION_BINDING_SCHEMA,
+    build_behavior_implementation_bindings,
+)
 from .integration import (
     enrich_asset_with_enterprise_understanding,
     install_enterprise_understanding_model,
@@ -22,10 +27,13 @@ __all__ = [
     "BEHAVIOR_SCHEMA",
     "BEHAVIOR_ROW_LEDGER_SCHEMA",
     "BEHAVIOR_GATE_SCHEMA",
+    "IMPLEMENTATION_BINDING_SCHEMA",
+    "IMPLEMENTATION_BINDING_GATE_SCHEMA",
     "build_decision_matrix_row_ledger",
     "build_business_behavior_ir",
     "build_governed_business_behavior_ir",
     "build_business_behavior_ir_v1",
+    "build_behavior_implementation_bindings",
     "build_enterprise_understanding_model",
     "assess_understanding_model",
     "enrich_asset_with_enterprise_understanding",
