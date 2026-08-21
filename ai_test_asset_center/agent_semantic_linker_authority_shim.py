81c7eae9fddbe3f5b0f06496914cf35a059d2715
mmc20210220-ctrl <mmc20210220@gmail.com>
2026-08-21 09:52:43 +0800

    chore(recall): add authority compatibility shim

---
+++ ai_test_asset_center/agent_semantic_linker_authority_shim.py
@@ -0,0 +1,8 @@
+"""Compatibility shim for the relationship-paged semantic-link authority."""
+from .agent_semantic_linker_authority_relationship_paging import *
+from .agent_semantic_linker_authority_relationship_paging import (
+    AgentSemanticLinkerError,
+    PROMPT_PROTOCOL,
+    RECEIPT_SCHEMA,
+    enrich_knowledge_asset_with_agent_relationships,
+)
